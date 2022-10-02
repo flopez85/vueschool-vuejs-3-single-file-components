cf16ed3aefdbf9347d4d3de8a1715bccf2c4d318
Francisco Lopez <loaf.85@gmail.com>
2022-10-02 15:44:08 -0700

    First Single File Component

---
+++ src/components/BigYellowUsername.vue
@@ -0,0 +1,21 @@
+<template>
+  <p class="username">{{ username }}</p>
+</template>
+
+<script>
+export default {
+  props: {
+    username: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="css">
+  .username {
+    font-size: 50pt;
+    color: #42b983;
+  }
+</style>
